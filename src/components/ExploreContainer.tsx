0a8c28a1f3d04fb933a20ed23362f9cd52d2f545
Mansor Gueye <mansourgueye275@gmail.com>
2020-05-27 21:25:13 +0200

    recherche vocale

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -4,8 +4,20 @@ import Livres from "./Booklist";
 import AjoutLivre from "./AjoutLivre";
 import { getBooks, getBooksFavori, addBook } from "../storage/db";
 import { Livre } from "../model/Livre";
-import {IonSearchbar, IonToolbar} from "@ionic/react"
+import {IonSearchbar, IonToolbar, IonIcon, IonButton} from "@ionic/react"
+import { micOutline } from "ionicons/icons";
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
 
+let options = {
+
+language: "fr-FR",
+matches:1,
+prompt:"dites le titre de livre à rechercher",
+showPopup: true,
+showPartial :true
+
+
+}
 interface ContainerProps {
   name: string;
 }
@@ -14,22 +26,47 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   const [livres, setLivres] = useState<Livre[]>([]);
   const [searchText, setSearchText] = useState("");
 
-  
     
   
-      getBooks().then((res) => setLivres(res));
+    getBooks().then((res) => setLivres(res));
    
     
 
   return (
     <>
       <IonToolbar>
+     
+    
         <IonSearchbar
           value={searchText}
           type="tel"
+          inputmode="text"
           placeholder="titre du livre"
           onIonChange={(e) => setSearchText(e.detail.value!)}
-        ></IonSearchbar>
+        > 
+         <IonButton onClick={()=>{ 
+          
+          // Request permissions
+
+          SpeechRecognition.hasPermission()
+          .then((permission : boolean)=>
+          { SpeechRecognition.requestPermission();})
+
+          SpeechRecognition.startListening(options)
+           .subscribe(
+    (matches: string[]) =>{ if (matches && matches.length>0){setSearchText(matches[0]);}},
+    (onerror) => alert('on a pas compris, reessayez svp !')
+  );
+ 
+  SpeechRecognition.stopListening();
+
+         }}>
+          <IonIcon icon={micOutline}>
+            </IonIcon>
+
+            </IonButton>   
+            </IonSearchbar>
+       
       </IonToolbar>
       <AjoutLivre nouvelleLivre={(livre) => addBook(livre).then(setLivres)} />
       <Livres livres={livres} searchtext={searchText}></Livres>
